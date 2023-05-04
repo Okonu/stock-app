842ff1318ef4c5be7ed8af572a1f11d24c0e5528
Okonu <ianyakundi015@gmail.com>
2023-05-05 00:20:58 +0300

    header template

---
+++ resources/views/header.blade.php
@@ -0,0 +1,46 @@
+<!-- start header menu -->
+
+<div class="top-menu">
+					<ul class="nav navbar-nav pull-right">
+						<li><a class="fullscreen-btn"><i data-feather="maximize"></i></a></li>
+						
+                        <!-- start manage user dropdown -->
+						<li class="dropdown dropdown-user">
+							<a class="dropdown-toggle" data-bs-toggle="dropdown" data-hover="dropdown"
+								data-close-others="true">
+								<img alt="" class="img-circle " src="../front/img/dp.jpg" />
+								<span class="username username-hide-on-mobile">ADMIN
+							</a>
+							<ul class="dropdown-menu dropdown-menu-default">
+								<li>
+									<a href="user_profile.html">
+										<i class="icon-user"></i> Profile </a>
+								</li>
+								<li>
+									<a href="#">
+										<i class="icon-settings"></i> Settings
+									</a>
+								</li>
+								<li>
+									<a href="#">
+										<i class="icon-directions"></i> Help
+									</a>
+								</li>
+								<li class="divider"> </li>
+								<li>
+									<a href="lock_screen.html">
+										<i class="icon-lock"></i> Lock
+									</a>
+								</li>
+								<li>
+									<a href="login.html">
+										<i class="icon-logout"></i> Log Out </a>
+								</li>
+							</ul>
+						</li>
+						<!-- end manage user dropdown -->
+					</ul>
+				</div>
+			</div>
+		</div>
+		<!-- end header -->
