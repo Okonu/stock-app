c54751616e2a9e6d660d2a387e2ac54b2cc451ef
Okonu <ianyakundi015@gmail.com>
2023-05-04 22:03:05 +0300

    dashboard template, removed login&auth, changed file structure

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\AdminController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,40 +13,8 @@ use App\Http\Controllers\AdminController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-// routes/web.php
-
-Route::get('admin/login', [App\Http\Controllers\AdminController::class, 'showLoginForm'])->name('admin.login');
-
-Route::post('admin/login', [App\Http\Controllers\AdminController::class, 'loginSubmit'])->name('admin.login.submit');
-
-Route::get('/admin/dashboard', [App\Http\Controllers\AdminController::class, 'showDashboard'])->name('admin.dashboard');
-
-Route::post('admin/create', [App\Http\Controllers\AdminController::class, 'createAdminUser'])->name('admin.createAdminUser');
-
-// Routes for user management
-Route::middleware(['auth'])->group(function () {
-    // Display user management dashboard
-    Route::get('/users', [UserController::class, 'index'])->name('users.index');
-
-    // Display form for creating a new user account
-    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-
-    // Store new user account in the database
-    Route::post('/users', [UserController::class, 'store'])->name('users.store');
-
-    // Display form for editing an existing user account
-    Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
-
-    // Update an existing user account in the database
-    Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
-
-    // Delete an existing user account from the database
-    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
-});
+Route::view('/', 'index');
