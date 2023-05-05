e3170e69eb8ba382578d4fdcd9f2516e9ee1df21
Okonu <ianyakundi015@gmail.com>
2023-05-05 05:47:51 +0300

    links

--- resources/views/display/addBay.blade.php
+++ resources/views/display/addBay.blade.php
@@ -11,9 +11,9 @@
 							</div>
 							<ol class="breadcrumb page-breadcrumb pull-right">
 								<li><i class="fa fa-home"></i>&nbsp;<a class="parent-item"
-										href="index-2.html">Home</a>&nbsp;<i class="fa fa-angle-right"></i>
+										href="#">Home</a>&nbsp;<i class="fa fa-angle-right"></i>
 								</li>
-								<li><a class="parent-item" href="#">Bays/Aisles</a>&nbsp;<i class="fa fa-angle-right"></i>
+								<li><a class="parent-item" href="{{url('/display/bays')}}">Bays/Aisles</a>&nbsp;<i class="fa fa-angle-right"></i>
 								</li>
 								<li class="active">Add Bays/Aisles</li>
 							</ol>
